68cde5facd7e5562fc8f84bc07a2aa15c2b1fee3
Jet Balagtas <jetbalagtas@gmail.com>
2019-03-13 18:29:43 -0400

    get index with the ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
 
   onToggleDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
